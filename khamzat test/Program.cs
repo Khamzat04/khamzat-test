972bd66adfdb6267a8cd6a33a3dc844f50aca768
Elev <Elev@TEB125103>
2023-01-27 15:00:24 +0100

    Add project files.

---
+++ khamzat test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Projekt_khamzat_test
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            double[] temperatur = new double[7];
+
+                 temperatur[0] = 4.5;
+                 temperatur[1] = 3.2;
+                 temperatur[2] = 6.7;
+                 temperatur[3] = 6.1;
+                 temperatur[4] = 2.1;
+                 temperatur[5] = 1.6;
+                 temperatur[6] = 2.9;
+
+            double sum = temperatur[0] + temperatur[1] + temperatur[2] + temperatur[3] + temperatur[4] + temperatur[5] + temperatur[6];
+
+                Console.WriteLine("medel temperaturen är:" + sum / 7);
+
+
+
+
+
+
+
+        }
+    }
+
+}
+            
